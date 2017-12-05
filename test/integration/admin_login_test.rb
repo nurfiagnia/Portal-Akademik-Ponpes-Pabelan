08dc6e8d584a47cbc7f419b0b180f6605aee260e
nurfiagnia <nurfiagnia.na@gmail.com>
2017-12-05 09:28:00 +0700

    testing admin

--- test/integration/admin_login_test.rb
+++ test/integration/admin_login_test.rb
@@ -5,10 +5,10 @@ class AdminLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "login with invalid information" do
+  test "login admin with invalid information" do
     get admin_path
     assert_template 'admin/login'
-    post admin_path, params: { session: { email: "", password: "" } }
+    post admin_path, params: { session: { username: "", password: "" } }
     assert_template 'admin/login'
     assert_not flash.empty?
     get admin_index_path
